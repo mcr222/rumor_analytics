efd2c4bc5edbf5d8b39c172afb1cb621cea22c5f
Miranti Rahmani <mirantirahmani@gmail.com>
2016-09-20 20:52:51 +0200

    crawling script v1

---
+++ crawl.py
@@ -0,0 +1,40 @@
+#__author: Miranti Rahmani__
+
+
+import tweepy #pip install tweepy
+from TwitterSearch import * #pip install TwitterSearch 
+
+
+auth = tweepy.OAuthHandler('yy2MNJhZohRNuLwmAGEpbxg29', 'YWplgn58vd5OAtJehKRQgCYe9Oi20YI01RwBFgkkGG2rSlv8Gi')
+auth.set_access_token('777483763597074432-ftrzRrPw2vBNyiADa02dAlcinNnYXSL', 'EELcpttFpHX74eTRx9GsKOYJmZqWJNWk6pgnSqvrTGp1Q')
+
+
+
+#search tweet
+try:
+	tso = TwitterSearchOrder()
+	tso.set_keywords(['tu', 'eindhoven']) #insert search keyword here
+
+	ts = TwitterSearch(
+	            consumer_key = 'yy2MNJhZohRNuLwmAGEpbxg29',
+	            consumer_secret = 'YWplgn58vd5OAtJehKRQgCYe9Oi20YI01RwBFgkkGG2rSlv8Gi',
+	            access_token = '777483763597074432-ftrzRrPw2vBNyiADa02dAlcinNnYXSL',
+	            access_token_secret = 'EELcpttFpHX74eTRx9GsKOYJmZqWJNWk6pgnSqvrTGp1Q'
+	        )
+
+	for tweet in ts.search_tweets_iterable(tso):
+	    print('@' + tweet['user']['screen_name'])
+	    print('id: ' + str(tweet['user']['id']))
+	    print(tweet['text'].encode('ascii','ignore') + '\n')
+
+except TwitterSearchException as e:
+    print(e)
+
+#----use this if you want to crawl from your own timeline----
+#get 20 tweets in my timeline
+#public_tweets = api.home_timeline()
+#file_out = open('output.txt', 'w')
+#for tweet in public_tweets:
+	#file_out.write(tweet.text.encode('ascii','ignore') + '\n')
+	#print tweet.text.encode('ascii','ignore') + '\n'
+
