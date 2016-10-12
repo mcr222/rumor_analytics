1662611c6587e395aa7c415ad5416e48812867df
mcr222 <markitus222@hotmail.com>
2016-10-12 12:41:40 +0200

    Added in the main code the call to crawl function with parameters

--- __main__.py
+++ __main__.py
@@ -2,5 +2,6 @@ from crawl import crawl
 from indexing import buildIndex
 
 
-if __name__ == "__main__":	
-	buildIndex(crawl())
+if __name__ == "__main__":
+	keyword = "football"
+	buildIndex(crawl(keyword))
