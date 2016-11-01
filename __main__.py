cf11f0971b8062ef1d64b292a37ea3a21ba3803f
mcr222 <markitus222@hotmail.com>
2016-11-01 09:32:27 +0100

    Updated clustering, and added more information to return from crawling and indexing. Also crawling merges several searches.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,19 @@
-from crawl import crawl
+from crawl import crawl, save_dictionary
 from indexing import buildIndex
 
 
 if __name__ == "__main__":
-	keyword = "futbol"
-	buildIndex(crawl(keyword))
+	keyword1 = "trump"
+	keyword2 = "vine"
+	diction, tweet_id_to_text, tweet_id_to_cluster = crawl(keyword1)
+	print len(diction)
+	print len(tweet_id_to_text)
+	tweet_id_to_cluster["num_elements_clust0"] = len(tweet_id_to_cluster)
+	diction, tweet_id_to_text, tweet_id_to_cluster = crawl(keyword2,diction, tweet_id_to_text,tweet_id_to_cluster,1)
+	tweet_id_to_cluster["num_elements_clust1"] = len(tweet_id_to_cluster)-tweet_id_to_cluster["num_elements_clust0"]
+	print len(diction)
+	print len(tweet_id_to_text)
+	tweet_id_to_cluster["num_clusters"] = 2
+	save_dictionary("tweet_text_dictionary.json", tweet_id_to_text)
+	save_dictionary("tweet_cluster_dictionary.json", tweet_id_to_cluster)
+	buildIndex(diction)
