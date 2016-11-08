0c8e548e83d7da3a458eed4b649ef445656a79a6
Belen <Belen@Belen-PC>
2016-11-08 19:05:13 +0100

    Comments on code

--- IndexTopicsWiki.py
+++ IndexTopicsWiki.py
@@ -1,6 +1,9 @@
 '''
 Created on 1/11/2016
-
+This class creates the dictionary of every term crawled in the wikipedia pages. 
+the output is the topics.csv file where each row corresponds to a term and its attributes are the frequency of this terms views in the topics pages.
+for instance:
+Trump: politics: 94758, footbal:2, beauty:348 etc. 
 @author: Belen
 '''
 
@@ -14,21 +17,21 @@ import WikiCrawlFootball
 import GeneralWikiCrawl
 
 
-dictionary = GeneralWikiCrawl.GeneralWikiCrawl()
+dictionary = GeneralWikiCrawl.GeneralWikiCrawl() #gets the dictionary from the Source file
 
 file_out = open('topics.csv', 'w')
-for term in sorted(dictionary):
+for term in sorted(dictionary): 
     if term[0][0].isalpha(): 
-        topics=dictionary[term]
+        topics=dictionary[term] #we store every term seen
         file_out.write(term+";")
         frequency_map={}
         iterator=0  
         for topic in topics:
             freqIni=frequency_map.get(topic,None)
             if(freqIni==None):
-                frequency_map[topic]=1
+                frequency_map[topic]=1 #stores its frequency of all the 13 topics. 
             else:
-                frequency_map[topic]=frequency_map[topic]+1
+                frequency_map[topic]=frequency_map[topic]+1 
         leng=len(frequency_map)      
         for topic in sorted(frequency_map):
             file_out.write(str(topic)+","+str(frequency_map[topic]))
