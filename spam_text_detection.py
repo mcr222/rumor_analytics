61d56b9efda8e0392d383f06e717eae51311acb5
Irfan Nur Afif <irfan.nur.95@gmail.com>
2016-11-08 15:34:19 +0100

    add dump tweets in spam detection

--- spam_text_detection.py
+++ spam_text_detection.py
@@ -1,16 +1,27 @@
+#author: Irfan Nur Afif
+
 from collections import Counter
 from nltk import NaiveBayesClassifier, classify
 import FilterStem
 import random
 from TwitterSearch import * #pip install TwitterSearch 
-import crawl
+import json
 
 error_count=0
 classifier_tweet={}
 classifier_sms={}
+#random string as initial feed to get unlabeled tweets for co-training
 random_string="hello world"
 
 
+
+def save_dictionary(name, save_dictionary):
+	f = open(name,"w")
+	json.dump(save_dictionary,f)
+	
+def read_dictionary(name):
+	f = open(name,"r")
+	return json.load(f)
 
 
 def get_features(text):
@@ -25,31 +36,33 @@ def get_features(text):
 def get_random_tweets():
     #still using dummy:
     global random_string
-    consumer_key='yy2MNJhZohRNuLwmAGEpbxg29'
-    consumer_secret='YWplgn58vd5OAtJehKRQgCYe9Oi20YI01RwBFgkkGG2rSlv8Gi'
-    access_token='777483763597074432-ftrzRrPw2vBNyiADa02dAlcinNnYXSL'
-    access_token_secret='EELcpttFpHX74eTRx9GsKOYJmZqWJNWk6pgnSqvrTGp1Q'
-    try:    
-        tso = TwitterSearchOrder()
-        #keyw=raw_input('Input search term:')
-        #tso.set_keywords([keyw]) #insert search keyword here
-        tso.set_keywords([random.choice(random_string.split())])
-        tso.set_language('en')
-
-        ts = TwitterSearch(
-                    consumer_key = consumer_key,
-                    consumer_secret = consumer_secret,
-                    access_token = access_token,
-                    access_token_secret = access_token_secret
-                )
+    #consumer_key='yy2MNJhZohRNuLwmAGEpbxg29'
+    #consumer_secret='YWplgn58vd5OAtJehKRQgCYe9Oi20YI01RwBFgkkGG2rSlv8Gi'
+    #access_token='777483763597074432-ftrzRrPw2vBNyiADa02dAlcinNnYXSL'
+    #access_token_secret='EELcpttFpHX74eTRx9GsKOYJmZqWJNWk6pgnSqvrTGp1Q'
+    # try:    
+    #     tso = TwitterSearchOrder()
+    #     tso.set_keywords([random.choice(random_string.split())])
+    #     tso.set_language('en')
+    #     tso.set_count(10)
+    #     ts = TwitterSearch(
+    #                 consumer_key = consumer_key,
+    #                 consumer_secret = consumer_secret,
+    #                 access_token = access_token,
+    #                 access_token_secret = access_token_secret
+    #             )
+    statuses=[]
+    try:
+     	statuses=read_dictionary("unlabeled_tweets")
+    except:
         statuses=[]
-        for tweet in ts.search_tweets_iterable(tso):
-            tweete=tweet['text'].replace('\n', ' ').encode('ascii','ignore')
-            statuses.append(tweete)
-            random_string=tweete
-            #print random_string
-    except TwitterSearchException as e: #catch errors
-        print(e)
+    #     for tweet in ts.search_tweets_iterable(tso):
+    #         tweete=tweet['text'].replace('\n', ' ').encode('ascii','ignore')
+    #         statuses.append(tweete)
+    #         random_string=tweete
+    # except TwitterSearchException as e: #catch errors
+    #     print("Error!")
+    save_dictionary("unlabeled_tweets",statuses)
     return statuses
         
 
@@ -124,28 +137,37 @@ def build_spam_classifier():
     global classifier_tweet
     classifier_sms = NaiveBayesClassifier.train(training_set)
     classifier_tweet = NaiveBayesClassifier.train(training_set_tweet)
-    
-    extra_data=[]
-    for i in xrange(1,50):
-        random_tweets=get_random_tweets()
-        for tweet in random_tweets:
-            sms_spam_prob=spam_sms_prob(tweet)
-            tweet_spam_prob=spam_tweet_prob(tweet)
-            feature = get_features(tweet)
-            if(sms_spam_prob>0.90 and tweet_spam_prob>0.90):
-                if feature is not None:
-                    extra_data.append((feature,"spam"))
-            if(sms_spam_prob<0.10 and tweet_spam_prob<0.10):
-                if feature is not None:
-                    extra_data.append((feature,"ham"))
-    #print(extra_data)
+    try:
+        extra_data=[]
+        for i in xrange(1,30):
+	    	counter=0
+	        random_tweets=get_random_tweets()
+	        for tweet in random_tweets:
+	            sms_spam_prob=spam_sms_prob(tweet)
+	            tweet_spam_prob=spam_tweet_prob(tweet)
+	            feature = get_features(tweet)
+	            if(sms_spam_prob>0.90 and tweet_spam_prob>0.90):
+	                if feature is not None and (feature,"spam") not in extra_data:
+	                    extra_data.append((feature,"spam"))
+	                    counter=counter+1
+	            if(sms_spam_prob<0.10 and tweet_spam_prob<0.10 and (feature,"ham") not in extra_data and counter>0):
+	                if feature is not None:
+	                    extra_data.append((feature,"ham"))
+	                    counter=counter-1
+	            	#classifier_sms = NaiveBayesClassifier.train(training_set)
+	            	#classifier_tweet = NaiveBayesClassifier.train(training_set_tweet)	    		
+    except TwitterSearchException as e: #catch errors
+	    print("")
+    training_set.extend(extra_data)
     training_set_tweet.extend(extra_data)
     classifier_tweet = NaiveBayesClassifier.train(training_set_tweet)
-    #classifier_tweet = NaiveBayesClassifier.train(training_set2)
-    ###print classifier_sms.show_most_informative_features(20)
-    #print classifier_tweet.show_most_informative_features(20)
-    ###print classify.util.accuracy(classifier_sms,test_set)
-    ###print classify.util.accuracy(classifier_sms,tweet_data)
+    #print classify.util.accuracy(classifier_sms,test_set)
+    #print classify.util.accuracy(classifier_sms,tweet_data)
+    #print classify.util.accuracy(classifier_tweet,test_set)
+    #print classify.util.accuracy(classifier_tweet,tweet_data)
+        ###print classifier_sms.show_most_informative_features(20)
+	    #print classifier_tweet.show_most_informative_features(20)
+    
 
                      
 #print sms_data[0:3]
