8c02d19b8d402125f79d604b873e5fbf1ec74e3e
Belen <Belen@Belen-PC>
2016-11-09 15:35:15 +0100

    Modified Labels function. now it controls cases whose freq is 0. + import division from future

--- Labels.py
+++ Labels.py
@@ -6,7 +6,7 @@ its purpose is that, given a list of elements (stemmed and filtered) as an input
 13 topics we have and will return the highest value normalized of every one. So the output will be a the higher ranked topic over the input 
 @author: Belen
 '''
-
+from __future__ import division
 import csv
 import FilterStem
 import math
@@ -30,6 +30,7 @@ def SearchLabel(input_list): #this is the function that, given a list, returns i
                     #Here we are going to calculate tf.idf for the input received over the corpus:dictionary
                     term_freq = item[i].split(',')[1] #frequency del topic 
                     term_freq = float(term_freq)
+                    
                     df = len(item)-1 #in how many docs the term appears
                     float(df)  
                     idf = (math.log(n_collections/df) ) #idf for every term 
@@ -37,7 +38,7 @@ def SearchLabel(input_list): #this is the function that, given a list, returns i
                     float(idf)                 
                     tf_idf = float(tf_idf)
                     list_of_words.append([topic, tf_idf])  
-    
+                    
     list_out=[]  
     #here we normalize the value of the tf.idf
     for top in topics:
@@ -46,21 +47,21 @@ def SearchLabel(input_list): #this is the function that, given a list, returns i
             if top == x[0]:
                 sum_topics=sum_topics + x[1]
         list_out.append([top, sum_topics])  
-          
-    print list_out
     
     sum_final= 0 
     for x in list_out:
         sum_final= sum_final + x[1] 
+        
       
     '''
     Return all non-zero topics an return and score from 0 to 1 by normalizing with the sum of all
     '''
     #we show results with a threshold of 0.1 
-    for output_values in list_out:
-        if output_values[1]/sum_final > 0.1:
-            print "%s : %f" % (output_values[0],output_values[1]/sum_final)
-        
-         
+    #If the terms were not present in any of the crawled pages, we display a 404 term not found. 
+    if sum_final != 0:
+        for output_values in list_out:
+            if output_values[1]/sum_final > 0.1:
+                print "%s : %f" % (output_values[0],output_values[1]/sum_final)
+    else: # we control the case where the term is not present in our dictionary 
+        print "404 - Terms not found in out Topics."
 
-        
